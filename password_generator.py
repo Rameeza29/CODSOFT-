017e4e47c8964debb8dba202f99b8099a37cdb4b
Rameeza29 <thattikantihasina@gmail.com>
2026-01-20 20:10:09 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,11 +1,18 @@
-import random
+import random  # Import the random module to generate random characters
 
-y= int(input("Enter length num: "))
-r = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-={}[]|\":;'<>?,./~`"
+y = int(input("Enter length num: "))  
+# Take user input for the desired password length and convert it to integer
 
-lst = []
+r = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-={}[]|\":;'<>?,./~`"  
+# String containing uppercase letters, lowercase letters, digits, and special characters
 
-for i in range(y):
-    lst.append(random.choice(r))
+lst = []  
+# Create an empty list to store randomly selected characters
 
-print("Password:", "".join(lst))
+for i in range(y):  
+    # Loop runs 'y' times (password length)
+    lst.append(random.choice(r))  
+    # Select a random character from string 'r' and add it to the list
+
+print("Password:", "".join(lst))  
+# Join all characters in the list into a single string and print the password
